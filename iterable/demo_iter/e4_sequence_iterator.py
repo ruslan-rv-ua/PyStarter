e29f92ea70c90477a50bba0249d9b912a66e4034
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2024-02-22 16:54:50 +0200

    Deployed 7c64eb3 with MkDocs version: 1.5.3

--- iterable/demo_iter/e4_sequence_iterator.py
+++ iterable/demo_iter/e4_sequence_iterator.py
@@ -1,7 +1,11 @@
-class Quads:
+class Squares:
+    def __init__(self, count):
+        self.index_range = range(count)
     def __getitem__(self, index):
-        if index in range(100):
-            return index * index
+        if index in self.index_range:
+            return (index+1)**2
         raise IndexError
 
-quads_iterator = iter(Quads())
+i = iter(Squares(3))
+# next(i)
+l = list(Squares(10))
