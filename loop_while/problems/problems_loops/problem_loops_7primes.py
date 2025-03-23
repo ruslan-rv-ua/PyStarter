1a479d2e9a5c66d8963de0eca99d9136a7bb845e
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2025-03-23 18:39:50 +0200

    Deployed 3622118 with MkDocs version: 1.6.1

--- loop_while/problems/problems_loops/problem_loops_7primes.py
+++ loop_while/problems/problems_loops/problem_loops_7primes.py
@@ -14,3 +14,18 @@
 13
 17
 '''
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+count = 0
+number = 2
+while count < 7:
+    if is_prime(number):
+        print(number)
+        count += 1
+    number += 1
